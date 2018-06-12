fb0dbac0ebc7466f23a88c57fc19f95030332e35
Laboralphy <raphael.marandet@gmail.com>
2018-06-12 12:44:06 +0200

    perlin map : added trees, waves, mounts, forest

--- examples/perlin/WorldGenerator.js
+++ examples/perlin/WorldGenerator.js
@@ -1,42 +1,5 @@
 
 
-const Perlin = O876.algorithms.Perlin;
-
-
-
-class IslandDistributor {
-	constructor() {
-		let pchan = new Perlin();
-		pchan.size(64);
-		this._perlin = pchan;
-	}
-
-	f(x, y) {
-		let data = this._perlin.generate(Math.floor(x / this._perlin.size()),Math.floor(y / this._perlin.size()));
-		return data
-				[WorldGenerator._mod(y, this._perlin.size())]
-				[WorldGenerator._mod(x, this._perlin.size())] > 0.7 ? 1 : 0;
-	}
-
-	render(oCanvas) {
-		let ctx = oCanvas.getContext('2d');
-        for (let y = 0, h = oCanvas.height; y < h; ++y) {
-			for (let x = 0, w = oCanvas.width; x < w; ++x) {
-				let v = this.f(x, y);
-				let c = v * 255 | 0;
-				let sStyle = O876.Rainbow.rgba({
-                    r: c,
-                    g: c,
-                    b: c,
-				});
-				ctx.fillStyle = sStyle;
-				ctx.fillRect(x, y, 1, 1);
-			}
-        }
-	}
-}
-
-
 class WorldGenerator {
 	constructor({cellSize, clusterSize, seed}) {
 		let pcell = new Perlin();
@@ -264,17 +227,22 @@ class WorldGenerator {
 				}
 			}
 		}));
+
+		ctx.strokeStyle = 'rgba(57, 25, 7, 0.5)';
+		ctx.fillStyle = 'rgba(57, 25, 7, 0.5)';
+		ctx.beginPath();
+		ctx.moveTo(0, c.height - 1);
+		ctx.lineTo(0, 0);
+		ctx.lineTo(c.width - 1, 0);
+		ctx.stroke();
+		ctx.fillText((xFrom + xCell) + ', ' + (yFrom + yCell), 10, 10);
 	}
 
 	renderClusterCells(oDestCanvas, xFrom, yFrom, xScreen, yScreen, wScreen, hScreen) {
 		let cellSize = this._perlinCell.size();
 		let c = this._canvasCell;
-		/**
-		 * @type {CanvasRenderingContext2D | WebGLRenderingContext}
-		 */
 		let ctx = c.getContext('2d');
-
-		ctx.font = '10px serif';
+		ctx.font = '10px italic serif';
 		wScreen = Math.ceil(wScreen / cellSize);
 		hScreen = Math.ceil(hScreen / cellSize);
 
@@ -283,148 +251,9 @@ class WorldGenerator {
 				let xCurs = xCell + xFrom;
 				let yCurs = yCell + yFrom;
 				let c = this._canvasCell;
-				let ctx = c.getContext('2d');
 				this.renderCell(xCurs, yCurs);
-				ctx.strokeStyle = 'rgba(57, 25, 7, 0.5)';
-				ctx.fillStyle = 'rgba(57, 25, 7, 0.5)';
-				ctx.beginPath();
-				ctx.moveTo(0, c.height - 1);
-				ctx.lineTo(0, 0);
-				ctx.lineTo(c.width - 1, 0);
-				ctx.stroke();
-				ctx.fillText((xFrom + xCell) + ', ' + (yFrom + yCell), 10, 10);
 				oDestCanvas.getContext('2d').drawImage(c, xScreen + xCell * cellSize, yScreen + yCell * cellSize);
 			}
 		}
 	}
 }
-
-
-
-class View {
-	constructor() {
-		this.xView = 0;
-		this.yView = 0;
-		this.wView = 32;
-		this.hView = 32;
-        this.WORLD_DEF = {
-            cellSize: 256,
-            clusterSize: 16,
-            seed: 0.111
-        };
-        let cvs = document.querySelector('.world');
-        this.world = new WorldGenerator(this.WORLD_DEF);
-        this.screen = WorldGenerator._canvas(cvs.width, cvs.height);
-        this.screenMap = WorldGenerator._canvas(32, 32);
-	}
-
-    render(oCanvas, xView, yView, x, y, w, h) {
-        this.world.renderClusterCells(
-            oCanvas,
-            xView, yView,
-            x, y,
-            w, h
-        );
-    }
-
-    view(x, y) {
-        this.xView = x;
-        this.yView = y;
-        this.render(this.screen, this.xView, this.yView, 0, 0, this.screen.width, this.screen.height);
-        this.commitScreen();
-    }
-
-	commitScreen() {
-        document.querySelector('.world').getContext('2d').drawImage(this.screen, 0, 0);
-	}
-
-    scrollLeft(n) {
-        let nCell = n * this.WORLD_DEF.cellSize;
-        let s = WorldGenerator._canvas(this.screen.width, this.screen.height);
-        s.getContext('2d').drawImage(
-            this.screen,
-            0, 0, this.screen.width - nCell, this.screen.height,
-            nCell, 0, this.screen.width - nCell, this.screen.height
-        );
-        this.render(s, this.xView -= n, this.yView, 0, 0, nCell, this.screen.height);
-        this.screen = s;
-        this.commitScreen();
-    }
-
-    scrollUp(n) {
-        let nCell = n * this.WORLD_DEF.cellSize;
-        let s = WorldGenerator._canvas(this.screen.width, this.screen.height);
-        s.getContext('2d').drawImage(
-            this.screen,
-            0, 0, this.screen.width, this.screen.height - nCell,
-            0, nCell, this.screen.width, this.screen.height - nCell
-        );
-        this.render(s, this.xView, this.yView -= n, 0, 0, this.screen.width, nCell);
-        this.screen = s;
-        this.commitScreen();
-    }
-
-    scrollRight(n) {
-        let nCell = n * this.WORLD_DEF.cellSize;
-        let s = WorldGenerator._canvas(this.screen.width, this.screen.height);
-        s.getContext('2d').drawImage(
-            this.screen,
-            nCell, 0, this.screen.width - nCell, this.screen.height,
-            0, 0, this.screen.width - nCell, this.screen.height
-        );
-        this.xView += n;
-        this.render(s, this.xView + (this.screen.width / this.WORLD_DEF.cellSize) - n, this.yView, this.screen.width - nCell, 0, nCell, this.screen.height);
-        this.screen = s;
-        this.commitScreen();
-    }
-
-    scrollDown(n) {
-        let nCell = n * this.WORLD_DEF.cellSize;
-        let s = WorldGenerator._canvas(this.screen.width, this.screen.height);
-        s.getContext('2d').drawImage(
-            this.screen,
-            0, nCell, this.screen.width, this.screen.height - nCell,
-            0, 0, this.screen.width, this.screen.height - nCell
-        );
-        this.yView += n;
-        this.render(s, this.xView, this.yView + (this.screen.height / this.WORLD_DEF.cellSize) - n, 0, this.screen.height - nCell, this.screen.width, nCell);
-        this.screen = s;
-        this.commitScreen();
-    }
-}
-
-
-
-function kbHandler(event) {
-    switch (event.key) {
-        case 'ArrowUp':
-            VIEW.scrollUp(1);
-            break;
-
-        case 'ArrowDown':
-            VIEW.scrollDown(1);
-            break;
-
-        case 'ArrowLeft':
-			VIEW.scrollLeft(1);
-            break;
-
-        case 'ArrowRight':
-            VIEW.scrollRight(1);
-            break;
-    }
-}
-
-let VIEW;
-function main() {
-	VIEW = new View();
-    VIEW.view(0, 0);
-    //let island = new IslandDistributor();
-    //island.render(document.querySelector('.world'));
-	console.log('done.');
-}
-
-
-
-window.addEventListener('load', main);
-document.addEventListener('keydown', kbHandler);
